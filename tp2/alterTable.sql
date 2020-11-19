98c1f37bf33f51de0b5ead98412a96c491181ddd
yanleclerc <65825575+yanleclerc@users.noreply.github.com>
2020-11-19 15:11:44 -0500

    ajout constraintes

--- tp2/alterTable.sql
+++ tp2/alterTable.sql
@@ -9,8 +9,8 @@ ADD CONSTRAINT typeCarte_Valide CHECK
 (typeCarte IN ('VISA', 'Master Card', 'American Express'));
 
 ALTER TABLE CommandeProduit
-ADD CONSTRAINT nbrItems_Positif CHECK (nbrItems >=0);
+ADD CONSTRAINT nbrItems_Valide CHECK (nbrItems >=0 AND nbrItems <=(SELECT Produit.stock FROM Produit WHERE Produit.numReference = commandeProduit.numReference));
 
 ALTER TABLE Fournisseur
-ADD CONSTRAINT typeFourn_valide CHECK
-(typeFourn IN ('Transformateur', 'Importateur', 'livreur'));
+ADD CONSTRAINT typeFourn_Valide CHECK
+(typeFourn IN ('Transformateur', 'Importateur', 'Livreur'));
