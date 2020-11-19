d6a33033684f361814d444fe3b71eed75333eb89
yanleclerc <65825575+yanleclerc@users.noreply.github.com>
2020-11-19 14:53:00 -0500

    Update alterTable.sql
    Ajout plusieurs constraints

--- tp2/alterTable.sql
+++ tp2/alterTable.sql
@@ -1,7 +1,16 @@
 ALTER TABLE Produit
 ADD CONSTRAINT stock_Positif CHECK (stock >= 0);
 
+ALTER TABLE Produit
+ADD CONSTRAINT seuilMinimum_Positif CHECK (seuilMinimum >=0);
+
 ALTER TABLE CarteCredit
 ADD CONSTRAINT typeCarte_Valide CHECK 
 (typeCarte IN ('VISA', 'Master Card', 'American Express'));
 
+ALTER TABLE CommandeProduit
+ADD CONSTRAINT nbrItems_Positif CHECK (nbrItems >=0);
+
+ALTER TABLE Fournisseur
+ADD CONSTRAINT typeFourn_valide CHECK
+(typeFourn IN ('Transformateur', 'Importateur', 'livreur'));
