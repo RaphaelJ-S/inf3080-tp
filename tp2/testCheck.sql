af2bbd4a96a9e0dd8cb36d16fe8f7b55bf7d755f
RaphaelJ-S <dk391069@ens.uqam.ca>
2020-11-22 13:34:28 -0500

    Ajout testCheck

---
+++ tp2/testCheck.sql
@@ -0,0 +1,11 @@
+INSERT INTO Produit
+VALUES(0, 0, 'xxx', 'xxx', 0.00, TO_DATE('2020-02-21'), 0,-1);
+
+INSERT INTO Produit 
+VALUES(0, 0, 'xxx', 'xxx' ,0.00, TO_DATE('2020-01-01'), -1, 0);
+
+INSERT INTO CarteCredit
+VALUES(102345223423, 'Test');
+
+INSERT INTO Fournisseur 
+VALUES(0, 'TEST', 'Pas bon');
