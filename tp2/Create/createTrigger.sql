ebb8a61d39b04da43e10803530d60dc3c32d1d99
RaphaelJ-S <dk391069@ens.uqam.ca>
2020-11-24 13:38:49 -0500

    ajout du premier trigger

---
+++ tp2/Create/createTrigger.sql
@@ -0,0 +1,19 @@
+create or replace TRIGGER actualiserStock
+BEFORE INSERT ON CommandeProduit 
+FOR EACH ROW
+DECLARE
+  qqt INTEGER;
+BEGIN
+  SELECT stock
+  INTO qqt
+  FROM Produit
+  WHERE numReference = :NEW.numreference;
+IF :new.nbritems > qqt then
+    raise_application_error(-20100, 'ERREUR DANS LE NBR');
+else 
+  UPDATE Produit SET stock = (stock - :new.nbritems) WHERE numReference = :new.numreference;
+end if;
+END;gneApres.numReference;
+  END IF;
+END;
+
