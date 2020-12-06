d85a6e96491566b25a7aa31eb8b52863e4af65e0
samkeroack <samuel_k_9@hotmail.com>
2020-12-06 13:24:33 -0500

    erreur de push

--- tp2/Create/createTable.sql
+++ tp2/Create/createTable.sql
@@ -1,89 +1,169 @@
+SET ECHO ON;
 
-create or replace procedure QuantiteDejaLivree(numRef in number,numCom in number)
-is
-nbr_items_c number(4);
-begin
+CREATE TABLE Produit(
+  numReference number(20) NOT NULL,
+  typeProduit number(5) NOT NULL,
+  description varchar2(20) NOT NULL,
+  preFix varchar2(10) NOT NULL,
+  prixVente number(10,2) NOT NULL,
+  dateEntree date NOT NULL,
+  seuilMinStock number(4) NOT NULL,
+  stock number(4) NOT NULL,
+  PRIMARY KEY(numReference)
+);
+--Livraison tmp, vérifier les autres références à cette table
+CREATE TABLE Livraisons(
+  numLivraison number(20),
+  numReference number(20) NOT NULL,
+  dateJour date NOT NULL,
+  dateLivraison date NOT NULL,
+  nbrItems number(10) NOT NULL,
+  PRIMARY KEY(numLivraison),
+  FOREIGN KEY(numReference) REFERENCES Produit ON DELETE CASCADE 
+);
+CREATE TABLE Adresse (
+  codePostal varchar2(7) NOT NULL,
+  pays varchar2(5) NOT NULL,
+  numCiv number(5) NOT NULL,
+  ville varchar2(15) NOT NULL, 
+  rue varchar2(20) NOT NULL,
+  PRIMARY KEY(codePostal)
+);
 
-select numLivraison into correspond from CommandeLivraison Where numCommande = numCom;
-select count(codeZebre) from Exemplaire WHERE numLivraison = correspond AND numReference = numRef;
-select nbrItems into nbr_items_c
-from CommandeLivraison where numRef=numReference and numCom =numCommande;
-dbms_output.put_line('Quantite deja livree: '|| nbr_items_c);
-end;
+CREATE TABLE Individu(
+  codeIndividu number(20) NOT NULL,
+  numTel varchar2(14) NOT NULL,
+  motDePasse varchar2(14) NOT NULL,
+  codePostal varchar2(7) NOT NULL,
+  PRIMARY KEY(codeIndividu),
+  FOREIGN KEY(codePostal) REFERENCES Adresse ON DELETE CASCADE
+);
 
 
-create or replace procedure TotalFacture(numFac in number)
-is
-montant_total_c number(10,2);
-begin
+CREATE TABLE Commande(
+  numCommande number(20) NOT NULL,
+  dateCommande date NOT NULL, 
+  etatCommande varchar2(10) NOT NULL,
+  codeIndividu number(20) NOT NULL,
+  PRIMARY KEY(numCommande),
+  FOREIGN KEY(codeIndividu) REFERENCES Individu ON DELETE CASCADE
+);
 
-select prixTotal into montant_total_c
-from Facture where numFac=numLivraison;
-dbms_output.put_line('Montant total de la facture: '|| montant_total_c);
-end;
+CREATE TABLE Paiement(
+  numPaiement number(20) NOT NULL,
+  numLivraison number(20) NOT NULL,
+  montant number(10,2) NOT NULL,
+  datePaiement date NOT NULL,
+  methodePaiement varchar2(10) NOT NULL,
+  PRIMARY KEY(numPaiement, numLivraison),
+  FOREIGN KEY(numLivraison) REFERENCES Livraisons ON DELETE CASCADE
+);
+CREATE TABLE CarteCredit(
+  numPaiement number(20),
+  numLivraison number(20),
+  dateExpiration date NOT NULL,
+  numCarte number(16) NOT NULL,
+  typeCarte varchar2(15) NOT NULL,
+  PRIMARY KEY(numPaiement, numLivraison),
+  FOREIGN KEY(numPaiement, numLivraison) REFERENCES Paiement ON DELETE CASCADE
+);
 
+CREATE TABLE Cheque(
+  numPaiement number(20),
+  numLivraison number(20),
+  numCheque number(20) NOT NULL,
+  idBanque number(10) NOT NULL,
+  PRIMARY KEY(numPaiement, numLivraison),
+  FOREIGN KEY(numPaiement, numLivraison) REFERENCES Paiement ON DELETE CASCADE
+);
 
-create or replace procedure ProduireFacture(numLivr in number, dateLimite in date)
-is
-num_client_c number(20);
-nom_client_c varchar(50);
-prenom_client_c varchar(50);
-num_livraison_c number(20);
-date_livraison_c date;
-prix_soustotal_c number(10,2);
-taxes_c number(10,2);
-prix_total_c number(10,2);
+CREATE TABLE CommandeProduit(
+  numReference number(20) NOT NULL,
+  numCommande number(20) NOT NULL,
+  nbrItems number(10) NOT NULL, 
+  PRIMARY KEY(numReference, numCommande),
+  FOREIGN KEY(numReference) REFERENCES Produit ON DELETE CASCADE,
+  FOREIGN KEY(numCommande) REFERENCES Commande ON DELETE CASCADE
+);  
 /*
-DECLARE
-CURSOR cur_adresse_client IS
-SELECT codePostal
-FROM Adresse
-WHERE codeIndividu =;  Facture.codeIndividu **FK de facture avec numLivr**
-adresse_complete Adresse.codePostal%TYPE;
-BEGIN
-  OPEN cur_adresse_client;
-LOOP
-  FETCH cur_adresse_client INTO adresse_complete;
-  EXIT WHEN cur_adresse_client%NOTFOUND;
-END LOOP
-CLOSE cur_adresse_client;
-END;
-
-DECLARE
-CURSOR liste_produit_facture IS
-SELECT X
-FROM X
-WHERE X = X; 
-liste_complete X.X%TYPE;
-BEGIN
-  OPEN liste_produit_facture;
-LOOP
-  FETCH liste_produit_facture INTO liste_complete;
-  EXIT WHEN liste_produit_facture%NOTFOUND;
-END LOOP
-CLOSE liste_produit_facture;
-END;
+CREATE TABLE ModificationPrix (
+  numMod number(20) not null,
+  dateMod date not null,
+  nouvPrix number(10,2) not null,
+  numReference number(20) not null,
+  foreign key(numReference) references Produit ON DELETE CASCADE,
+  primary key(numMod)
+);
 */
-begin
+CREATE TABLE Fournisseur(
+  codeIndividu number(20) NOT NULL,
+  typeFourn varchar2(50) NOT NULL,
+  attribut varchar2(50) NOT NULL,
+  PRIMARY KEY(codeIndividu),
+  FOREIGN KEY(codeIndividu) REFERENCES Individu ON DELETE CASCADE
+);
 
-select CODEINDIVIDU into num_client_c from Facture Where numLivraison = numLivr;
-select nom into nom_client_c from Client INNER JOIN Facture ON Client.codeIndividu = Facture.CODEINDIVIDU;
-select prenom into prenom_client_c from Client INNER JOIN Facture ON Client.codeIndividu = Facture.CODEINDIVIDU;
-select numLivraison into num_livraison_c from Facture Where numLivraison = numLivr;
-select DATELIVRAISON into date_livraison_c from Livraison Where numLivraison = numLivr;
-select prixSousTotal into prix_soustotal_c from Facture Where numLivraison = numLivr;
-select taxes into taxes_c from Facture Where numLivraison = numLivr;
-select prixTotal into prix_total_c from Facture Where numLivraison = numLivr;
+CREATE TABLE Client(
+  codeIndividu number(20) NOT NULL,
+  nom varchar2(50) NOT NULL,
+  prenom varchar2(50) NOT NULL,
+  qualite varchar2(50) NOT NULL,
+  etatCompte varChar(10) NOT NULL,
+  PRIMARY KEY(codeIndividu),
+  FOREIGN KEY(codeIndividu) REFERENCES Individu ON DELETE CASCADE
+);
 
-dbms_output.put_line('**********Facture Client**********');
-dbms_output.put_line('Numero du Client: '|| num_client_c);
-dbms_output.put_line('Nom du Client: '|| nom_client_c);
-dbms_output.put_line('Prenom du Client: '|| prenom_client_c);
-dbms_output.put_line('Adresse du Client: '|| adresse_complete);
-dbms_output.put_line('Numero de Livraison: '|| num_livraison_c);
-dbms_output.put_line('Date de Livraison: '|| date_livraison_c);
-dbms_output.put_line('Liste de Produits: '|| liste_complete);
-dbms_output.put_line('Prix Sous-Total: '|| prix_soustotal_c);
-dbms_output.put_line('Montant des Taxes: '|| taxes_c);
-dbms_output.put_line('Prix Total: '|| prix_total_c);
-end;
+CREATE TABLE CommandeLivraison(
+  numCommande number(20),
+  numLivraison number(20),
+  PRIMARY KEY(numCommande, numLivraison),
+  FOREIGN KEY(numCommande) REFERENCES Commande ON DELETE CASCADE,
+  FOREIGN KEY(numLivraison) REFERENCES Livraisons ON DELETE CASCADE
+);
+CREATE TABLE ProduitFournisseur(
+  codeIndividu number(20) NOT NULL,
+  numReference number(20) NOT NULL,
+  ordrePrio number(1) NOT NULL,
+  PRIMARY KEY(codeIndividu, numReference),
+  FOREIGN KEY(codeIndividu) REFERENCES Fournisseur ON DELETE CASCADE,
+  FOREIGN KEY(numReference) REFERENCES Produit ON DELETE CASCADE
+);
+
+CREATE TABLE Facture(
+  numLivraison number(20) NOT NULL,
+  prixSousTotal number(10,2) NOT NULL,
+  taxes number(10,2) NOT NULL,
+  prixTotal number(10,2) NOT NULL,
+  etatFacture varchar2(10) NOT NULL,
+  codeIndividu number(20) NOT NULL,
+  PRIMARY KEY(numLivraison),
+  FOREIGN KEY(codeIndividu) REFERENCES Client ON DELETE CASCADE,
+  FOREIGN KEY(numLivraison) REFERENCES Livraisons ON DELETE CASCADE
+);
+
+CREATE TABLE Exemplaire (
+  codeZebre number(12) NOT NULL,
+  numReference number(20) NOT NULL,
+  numLivraison number(20) NOT NULL,
+  PRIMARY KEY(codeZebre),
+  FOREIGN KEY(numReference) REFERENCES Produit ON DELETE CASCADE,
+  FOREIGN KEY(numLivraison) REFERENCES Livraisons ON DELETE CASCADE
+);
+
+
+ALTER TABLE Produit
+ADD CONSTRAINT stock_Positif CHECK (stock >= 0);
+
+ALTER TABLE Produit
+ADD CONSTRAINT seuilMinimum_Positif CHECK (seuilMinStock >= 0);
+
+ALTER TABLE CarteCredit
+ADD CONSTRAINT typeCarte_Valide CHECK 
+(typeCarte IN ('VISA', 'Master Card', 'American Express'));
+
+ALTER TABLE CommandeProduit
+ADD CONSTRAINT nbrItems_Valide CHECK (nbrItems >=0);
+
+ALTER TABLE Fournisseur
+ADD CONSTRAINT typeFourn_Valide CHECK
+(typeFourn IN ('Transformateur', 'Importateur', 'Livreur'));
