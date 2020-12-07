1ca5fcf691c749dac104201ae907180b2034bf2a
samkeroack <samuel_k_9@hotmail.com>
2020-12-07 11:14:13 -0500

    Avancé la fct produireFacture

--- tp2/Create/createProcFonc.sql
+++ tp2/Create/createProcFonc.sql
@@ -5,7 +5,7 @@ create or replace procedure QuantiteDejaLivree(numRef in number, numCom in numbe
 
 begin
 
-    select numLivraison into correspond from CommandeLivraison Where numCommande = numCom;
+    select numLivraison into correspond from LIVRAISON Where NUMCOMMANDE = numCom;
     select count(codeZebre) into nbr_items_c from Exemplaire WHERE numLivraison = correspond AND numReference = numRef;
     /*select nbrItems
     into nbr_items_c
@@ -34,78 +34,116 @@ create or replace procedure ProduireFacture(numLivr in number, dateLimite in dat
     num_client_c     number(20);
     nom_client_c     varchar(50);
     prenom_client_c  varchar(50);
+    adresse_complete Adresse%rowtype;
     num_livraison_c  number(20);
     date_livraison_c date;
     prix_soustotal_c number(10, 2);
     taxes_c          number(10, 2);
     prix_total_c     number(10, 2);
+    c_num_commande   number(20);
+    code_zebre_c     number(12);
+
+Begin
+    DECLARE
+        CURSOR cur_liste_commande IS
+            SELECT NUMCOMMANDE
+            into c_num_commande
+            FROM LIVRAISON
+            WHERE NUMLIVRAISON = numLivr;
+        produits_commandes NUMCOMMANDE%TYPE;
+    BEGIN
+        OPEN cur_liste_commande;
+        LOOP
+            FETCH cur_liste_commande INTO produits_commandes;
+            EXIT WHEN cur_liste_commande%NOTFOUND;
+        END LOOP;
+        CLOSE cur_liste_commande;
 
 
-/*DECLARE
-CURSOR cur_produit_facture IS
-SELECT *
-FROM PRODUIT
-WHERE  = numLivr
-liste_complete %TYPE;
-BEGIN
-  OPEN liste_produit_facture;
-LOOP
-  FETCH liste_produit_facture INTO liste_complete;
-  EXIT WHEN liste_produit_facture%NOTFOUND;
-END LOOP
-CLOSE liste_produit_facture;
-*/
+        DECLARE
+            CURSOR cur_liste_produit IS
+                SELECT *
+                FROM COMMANDEPRODUIT
+                WHERE c_num_commande = NUMCOMMANDE;
+            produits_commandes COMMANDEPRODUIT%ROWTYPE;
+        BEGIN
+            OPEN cur_liste_produit;
+            LOOP
+                FETCH cur_liste_produit INTO produits_commandes;
+                EXIT WHEN cur_liste_produit%NOTFOUND;
+            END LOOP;
+            CLOSE cur_liste_produit;
+
+            DECLARE
+                CURSOR cur_info_produit IS
+                    SELECT *
+                    FROM PRODUIT
+                    WHERE produits_commandes.NUMREFERENCE = PRODUIT.NUMREFERENCE;
+                info_produits PRODUIT%ROWTYPE;
+            BEGIN
+                OPEN cur_info_produit;
+                LOOP
+                    FETCH cur_info_produit INTO info_produits;
+                    EXIT WHEN cur_info_produit%NOTFOUND;
+                END LOOP;
+                CLOSE cur_info_produit;
+
+                select CODEZEBRE
+                into code_zebre_c
+                FROM EXEMPLAIRE
+                where NUMREFERENCE = info_produits.NUMREFERENCE;
 
 
-DECLARE
-    CURSOR cur_adresse_client IS
-        SELECT *
-        FROM Adresse
-        where ADRESSE.CODEINDIVIDU = (select CODEINDIVIDU
-                                      from Facture
-                                      Where numLivraison = numLivr
-        );
-    adresse_complete ADRESSE%ROWTYPE;
+                SELECT *
+                into adresse_complete
+                FROM Adresse
+                where ADRESSE.CODEINDIVIDU = (select CODEINDIVIDU
+                                              from Facture
+                                              Where numLivraison = numLivr
+                );
 
-BEGIN
-    OPEN cur_adresse_client;
-    LOOP
-        FETCH cur_adresse_client INTO adresse_complete;
-        EXIT WHEN cur_adresse_client%NOTFOUND;
-    END LOOP;
-    CLOSE cur_adresse_client;
+                select CODEINDIVIDU into num_client_c from Facture Where numLivraison = numLivr;
 
-    select CODEINDIVIDU into num_client_c from Facture Where numLivraison = numLivr;
+                select nom
+                into nom_client_c
+                from Client
+                         INNER JOIN Facture ON Client.codeIndividu = Facture.CODEINDIVIDU;
 
-    select nom
-    into nom_client_c
-    from Client
-             INNER JOIN Facture ON Client.codeIndividu = Facture.CODEINDIVIDU;
+                select prenom
+                into prenom_client_c
+                from Client
+                         INNER JOIN Facture ON Client.codeIndividu = Facture.CODEINDIVIDU;
 
-    select prenom
-    into prenom_client_c
-    from Client
-             INNER JOIN Facture ON Client.codeIndividu = Facture.CODEINDIVIDU;
+                select numLivraison into num_livraison_c from Facture Where numLivraison = numLivr;
 
-    select numLivraison into num_livraison_c from Facture Where numLivraison = numLivr;
+                select DATELIVRAISON into date_livraison_c from Livraison Where numLivraison = numLivr;
 
-    select DATELIVRAISON into date_livraison_c from Livraison Where numLivraison = numLivr;
+                select prixSousTotal into prix_soustotal_c from Facture Where numLivraison = numLivr;
 
-    select prixSousTotal into prix_soustotal_c from Facture Where numLivraison = numLivr;
+                select taxes into taxes_c from Facture Where numLivraison = numLivr;
 
-    select taxes into taxes_c from Facture Where numLivraison = numLivr;
+                select prixTotal into prix_total_c from Facture Where numLivraison = numLivr;
 
-    select prixTotal into prix_total_c from Facture Where numLivraison = numLivr;
-
-    dbms_output.put_line('**********Facture Client**********');
-    dbms_output.put_line('Numero du Client: ' || num_client_c);
-    dbms_output.put_line('Nom du Client: ' || nom_client_c);
-    dbms_output.put_line('Prenom du Client: ' || prenom_client_c);
-    dbms_output.put_line('Adresse du Client: ' || adresse_complete);
-    dbms_output.put_line('Numero de Livraison: ' || num_livraison_c);
-    dbms_output.put_line('Date de Livraison: ' || date_livraison_c);
-/*dbms_output.put_line('Liste de Produits: ' || liste_complete);*/
-    dbms_output.put_line('Prix Sous-Total: ' || prix_soustotal_c);
-    dbms_output.put_line('Montant des Taxes: ' || taxes_c);
-    dbms_output.put_line('Prix Total: ' || prix_total_c);
-end;
+                dbms_output.put_line('**********Facture Client**********');
+                dbms_output.put_line('Numero du Client: ' || num_client_c);
+                dbms_output.put_line('Nom du Client: ' || nom_client_c);
+                dbms_output.put_line('Prenom du Client: ' || prenom_client_c);
+                dbms_output.put_line('Adresse du Client ');
+                dbms_output.put_line('Numero Civique: ' || adresse_complete.NUMCIV);
+                dbms_output.put_line('Rue: ' || adresse_complete.RUE);
+                dbms_output.put_line('Code Postal: ' || adresse_complete.CODEPOSTAL);
+                dbms_output.put_line('Ville: ' || adresse_complete.Ville);
+                dbms_output.put_line('Pays: ' || adresse_complete.Pays);
+                dbms_output.put_line('Numero de Livraison: ' || num_livraison_c);
+                dbms_output.put_line('Date de Livraison: ' || date_livraison_c);
+                /*      dbms_output.put_line('#Produit : ' || info_produits.TYPEPRODUIT);
+                      DBMS_OUTPUT.put_line('Code Zebre : ' || code_zebre_c);
+                      dbms_output.put_line('#Commande : ' || produits_commandes.NUMCOMMANDE);
+                      dbms_output.put_line('Prix : ' || info_produits.PRIXVENTE);
+                 */ dbms_output.put_line('Prix Sous-Total: ' || prix_soustotal_c);
+                dbms_output.put_line('Montant des Taxes: ' || taxes_c);
+                dbms_output.put_line('Prix Total: ' || prix_total_c);
+            end;
+            end;
+        end;
+    end;
