54be7475d7cc5df496a94f00a74c0bbce61afb70
samkeroack <samuel_k_9@hotmail.com>
2020-12-08 12:03:13 -0500

    maj

--- tp2/Create/createProcFonc.sql
+++ tp2/Create/createProcFonc.sql
@@ -28,23 +28,38 @@ begin
 end;
 /
 
-create procedure ProduireFacture(numLivr in number, dateLimite_f in date)
+create or replace procedure ProduireFacture(numLivr in number, dateLimite_f in date)
     is
-    num_client_c     number(20);
-    nom_client_c     varchar(50);
-    prenom_client_c  varchar(50);
-    num_livraison_c  number(20);
-    date_livraison_c date;
-    prix_soustotal_c number(10, 2);
-    taxes_c          number(10, 2);
-    prix_total_c     number(10, 2);
-    c_num_commande   number(20);
-    dateLimite_c     date;
-    e_rue            varchar(10) ;
-    e_ville          varchar(10) ;
-    e_numCiv         varchar(10) ;
-    e_pays           varchar(10) ;
-    e_cp             varchar(10) ;
+
+    num_client_c       number(20);
+    nom_client_c       varchar(50);
+    prenom_client_c    varchar(50);
+    num_livraison_c    number(20);
+    date_livraison_c   date;
+    prix_soustotal_c   number(10, 2);
+    taxes_c            number(10, 2);
+    prix_total_c       number(10, 2);
+    dateLimite_c       date;
+    e_rue              varchar(10) ;
+    e_ville            varchar(10) ;
+    e_numCiv           varchar(10) ;
+    e_pays             varchar(10) ;
+    e_cp               varchar(10) ;
+    c_num_livraison    int ;
+    c_type_produit     varchar(20) ;
+    c_prix_vente       varchar(20) ;
+    c_code_zebre       varchar(20) ;
+    c_num_commande     varchar(20);
+    CURSOR cur_liste_commande IS
+        SELECT LIVRAISONS.NUMLIVRAISON, LIVRAISONS.NUMCOMMANDE, CODEZEBRE, PRIXVENTE, TYPEPRODUIT
+        into c_num_livraison, c_num_commande, c_code_zebre, c_prix_vente, c_type_produit
+        FROM LIVRAISONS
+                 INNER JOIN COMMANDEPRODUIT C2 on LIVRAISONS.NUMCOMMANDE = C2.NUMCOMMANDE
+                 INNER JOIN PRODUIT P on P.NUMREFERENCE = C2.NUMREFERENCE
+                 INNER JOIN EXEMPLAIRE E on LIVRAISONS.NUMLIVRAISON = E.NUMLIVRAISON
+        WHERE LIVRAISONS.NUMLIVRAISON = 2;
+    produits_commandes cur_liste_commande%ROWTYPE;
+
 begin
 
     SELECT codePostal, pays, numCiv, ville, rue
@@ -80,7 +95,8 @@ begin
 
     select prixTotal into prix_total_c from Facture Where numLivraison = numLivr;
 
-    select datepayerlim into dateLimite_c from FACTURE where datepayerLim = dateLimite_f;
+    select datepayerLim into dateLimite_c from FACTURE where datepayerlim = dateLimite_f;
+                                                                                                                               
 
     dbms_output.put_line('**********Facture Client**********');
     dbms_output.put_line('Numero du Client: ' || num_client_c);
@@ -95,34 +111,16 @@ begin
     dbms_output.put_line('Numero de Livraison: ' || num_livraison_c);
     dbms_output.put_line('Date de Livraison: ' || date_livraison_c);
 
-
-    DECLARE
-        c_num_livraison    int ;
-        c_type_produit     varchar(20) ;
-        c_prix_vente       varchar(20) ;
-        c_code_zebre       varchar(20) ;
-        c_num_commande     varchar(20);
-        CURSOR cur_liste_commande IS
-            SELECT LIVRAISONS.NUMLIVRAISON, LIVRAISONS.NUMCOMMANDE, CODEZEBRE, PRIXVENTE, TYPEPRODUIT
-            into c_num_livraison, c_num_commande, c_code_zebre, c_prix_vente, c_type_produit
-            FROM LIVRAISONS
-                     INNER JOIN COMMANDEPRODUIT C2 on LIVRAISONS.NUMCOMMANDE = C2.NUMCOMMANDE
-                     INNER JOIN PRODUIT P on P.NUMREFERENCE = C2.NUMREFERENCE
-                     INNER JOIN EXEMPLAIRE E on LIVRAISONS.NUMLIVRAISON = E.NUMLIVRAISON
-            WHERE LIVRAISONS.NUMLIVRAISON = 2;
-        produits_commandes cur_liste_commande%ROWTYPE;
-    BEGIN
-        OPEN cur_liste_commande;
-        LOOP
-            FETCH cur_liste_commande INTO produits_commandes;
-            dbms_output.put_line('#Produit : ' || produits_commandes.TYPEPRODUIT);
-            DBMS_OUTPUT.put_line('Code Zebre : ' || produits_commandes.CODEZEBRE);
-            dbms_output.put_line('#Commande : ' || produits_commandes.NUMCOMMANDE);
-            dbms_output.put_line('Prix : ' || produits_commandes.PRIXVENTE);
-            EXIT WHEN cur_liste_commande%NOTFOUND;
-        END LOOP;
-        CLOSE cur_liste_commande;
-    end;
+    OPEN cur_liste_commande;
+    LOOP
+        FETCH cur_liste_commande INTO produits_commandes;
+        dbms_output.put_line('#Produit : ' || produits_commandes.TYPEPRODUIT);
+        DBMS_OUTPUT.put_line('Code Zebre : ' || produits_commandes.CODEZEBRE);
+        dbms_output.put_line('#Commande : ' || produits_commandes.NUMCOMMANDE);
+        dbms_output.put_line('Prix : ' || produits_commandes.PRIXVENTE);
+        EXIT WHEN cur_liste_commande%NOTFOUND;
+    END LOOP;
+    CLOSE cur_liste_commande;
 
     DBMS_OUTPUT.PUT_LINE('Date Limite de paiement : ' || dateLimite_c);
     dbms_output.put_line('Prix Sous-Total: ' || prix_soustotal_c);
@@ -130,6 +128,7 @@ begin
     dbms_output.put_line('Prix Total: ' || prix_total_c);
 end;
 /
+
 
 
 
